e788b481b3afd1d888815a040ce5208caed9ed16
Atilla Nalcaci <atillanalcaci@sabanciuniv.edu>
2019-04-02 21:15:23 +0300

    Update K-NearestNeighbors.py

--- Codes/K-NearestNeighbors.py
+++ Codes/K-NearestNeighbors.py
@@ -1,5 +1,9 @@
 import random
 import csv
+import math
+import operator
+
+
 def loadDataset(filename, split, trainingSet=[] , testSet=[]):
 	with open(filename, 'rt', encoding='utf-8') as csvfile:
 	    lines = csv.reader(csvfile)
@@ -12,17 +16,14 @@ def loadDataset(filename, split, trainingSet=[] , testSet=[]):
 	        else:
 	            testSet.append(dataset[x])
 	
-
-
-
-import math
+	
 def euclideanDistance(instance1, instance2, length):
 	distance = 0
 	for x in range(length):
 		distance += pow((float(instance1[x]) - float(instance2[x])), 2)
 	return math.sqrt(distance)
 
-import operator 
+
 def getNeighbors(trainingSet, testInstance, k):
 	distances = []
 	length = len(testInstance)-1
@@ -35,7 +36,7 @@ def getNeighbors(trainingSet, testInstance, k):
 		neighbors.append(distances[x][0])
 	return neighbors
 
-import operator
+
 def getResponse(neighbors):
 	classVotes = {}
 	for x in range(len(neighbors)):
@@ -47,12 +48,14 @@ def getResponse(neighbors):
 	sortedVotes = sorted(classVotes.items(), key=operator.itemgetter(1), reverse=True)
 	return sortedVotes[0][0]
 
+
 def getAccuracy(testSet, predictions):
 	correct = 0
 	for x in range(len(testSet)):
 		if testSet[x][-1] is predictions[x]:
 			correct += 1
 	return (correct/float(len(testSet))) * 100.0
+
 
 def main():
 	# prepare data
@@ -75,5 +78,6 @@ def main():
 		print('> predicted=' + repr(result) + ', actual=' + repr(testSet[x][-1]))
 	accuracy = getAccuracy(testSet, predictions)
 	print('Accuracy: ' + repr(accuracy) + '%')
+
 	
 main()
